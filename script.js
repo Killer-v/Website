910fc080238478529a186cd528ec9e21e4d881e4
Violetta <88794494+Viola-lol@users.noreply.github.com>
2022-11-10 20:54:09 +0200

    fix gradient in portfolio

--- script.js
+++ script.js
@@ -10,16 +10,31 @@ switchMode.onclick = function () {
     }
 }
 
-const handleOnMouseMove = e => {
-    const { currentTarget: target } = e;
+// const handleOnMouseMove = e => {
+//     const { currentTarget: target } = e;
 
-    const rect = target.getBoundingClientRect(),
-          x = e.clientX - rect.left,
-          y = e.clientY - rect.top;
-    target.style.setProperty("--mouse-x", `${x}px`);
-    target.style.setProperty("--mouse-y", `${y}px`);
-}
+//     const rect = target.getBoundingClientRect(),
+//         x = e.clientX - rect.left,
+//         y = e.clientY - rect.top;
+//     target.style.setProperty("--mouse-x", `${x}px`);
+//     target.style.setProperty("--mouse-y", `${y}px`);
+// }
 
-for(const card of document.querySelectorAll(".card")) {
-    card.onmousemove = e => handleOnMouseMove(e);
+// for (const card of document.querySelectorAll(".website")) {
+//     card.onmousemove = e => handleOnMouseMove(e);
+// }
+
+document.getElementById("websites").onmousemove = e => {
+    for (const card of document.getElementsByClassName("website")) {
+        const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+    }
+    for (const card of document.querySelectorAll(".website")) {
+            card.onmousemove = e => handleOnMouseMove(e);
+
+            };
 }
