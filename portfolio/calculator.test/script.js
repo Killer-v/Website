88a6b4b92e18e0d1e221ae77470d9d4e3187db22
Dmytro Soldatov <cyberdexx@gmail.com>
2023-05-25 15:35:20 +0300

    update

--- portfolio/calculator.test/script.js
+++ portfolio/calculator.test/script.js
@@ -1,167 +1,195 @@
-function createMainDiv() {
-    const div = document.createElement("div");
-    div.className = "calculator-box";
-    document.body.appendChild(div);
+const buttonsList = [
+  { id: "as", class: "btn as bg-grey", value: "as" },
+  { id: "plusMinus", class: "btn plusMinus bg-grey", value: "+/-" },
+  { id: "percent", class: "btn percent bg-grey", value: "%" },
+  { id: "division", class: "btn division bg-orange", value: "/" },
+  { id: "seven", class: "btn seven", value: "7" },
+  { id: "eight", class: "btn eight", value: "8" },
+  { id: "nine", class: "btn nine", value: "9" },
+  { id: "myltiply", class: "btn myltiply bg-orange", value: "X" },
+  { id: "four", class: "btn four", value: "4" },
+  { id: "five", class: "btn five", value: "5" },
+  { id: "six", class: "btn six", value: "6" },
+  { id: "minus", class: "btn minus bg-orange", value: "-" },
+  { id: "one", class: "btn one", value: "1" },
+  { id: "two", class: "btn two", value: "2" },
+  { id: "three", class: "btn three", value: "3" },
+  { id: "plus", class: "btn plus bg-orange", value: "+" },
+  { id: "zero", class: "btn zero", value: "0" },
+  { id: "dot", class: "btn dot", value: "." },
+  { id: "equal", class: "btn equal bg-orange", value: "=" },
+];
 
-    return div;
-};
+let number1 = null;
+let number2 = null;
+let operation = null;
+
+function createMainDiv() {
+  const div = document.createElement("div");
+  div.className = "calculator-box";
+
+  return div;
+}
 
 function createInput() {
-    const input = document.createElement("input");
-    input.className = "conclusion";
-    input.type = "text";
-    input.value = 0;
-    input.onclick = () => { input.value = '' }
-    input.onchange = () => inputChange();
-    input.onkeydown = (event) => {
-        event.key === "Enter" && getResult();
-    }
+  const input = document.createElement("input");
 
-    calculatorDiv.appendChild(input);
+  input.className = "conclusion";
+  input.type = "text";
+  input.value = 0;
+  input.readOnly = true;
 
-    return input;
-};
+  return input;
+}
 
-function createButtons() {
-    const buttonsDiv = document.createElement("div");
-    buttonsDiv.className = "button-box";
+function createButtonsDiv() {
+  const buttonsDiv = document.createElement("div");
+  buttonsDiv.className = "button-box";
 
-    const buttonsList = [
-        { id: "as", class: "btn as bg-grey", value: "as" },
-        { id: "plusMinus", class: "btn plusMinus bg-grey", value: "+/-" },
-        { id: "percent", class: "btn percent bg-grey", value: "%" },
-        { id: "division", class: "btn division bg-orange", value: "/" },
-        { id: "seven", class: "btn seven", value: "7" },
-        { id: "eight", class: "btn eight", value: "8" },
-        { id: "nine", class: "btn nine", value: "9" },
-        { id: "myltiply", class: "btn myltiply bg-orange", value: "X" },
-        { id: "four", class: "btn four", value: "4" },
-        { id: "five", class: "btn five", value: "5" },
-        { id: "six", class: "btn six", value: "6" },
-        { id: "minus", class: "btn minus bg-orange", value: "-" },
-        { id: "one", class: "btn one", value: "1" },
-        { id: "two", class: "btn two", value: "2" },
-        { id: "three", class: "btn three", value: "3" },
-        { id: "plus", class: "btn plus bg-orange", value: "+" },
-        { id: "zero", class: "btn zero", value: "0" },
-        { id: "dot", class: "btn dot", value: "." },
-        { id: "equal", class: "btn equal bg-orange", value: "=" },
-    ];
+  return buttonsDiv;
+}
 
-    const buttons = {};
+function createButton(buttonData) {
+  const button = document.createElement("button");
+  button.className = buttonData.class;
+  button.textContent = buttonData.value;
+  button.id = buttonData.id;
 
-    buttonsList.forEach((scan) => {
-        const button = document.createElement("button");
-        button.className = scan.class;
-        button.textContent = scan.value;
-        button.id = scan.id;
+  return button;
+}
 
-        buttons[scan.id] = button;
+function createButtons(buttonsList, buttonsDiv) {
+  buttonsList.forEach((buttonData) => {
+    const button = createButton(buttonData);
 
-        button.onclick = () => onPress(scan.value);
+    button.onclick = () => onButtonPress(buttonData.value);
 
-        buttonsDiv.appendChild(button);
+    buttonsDiv.appendChild(button);
+  });
+}
 
-    });
+function onNumberPressed(number) {
+  if (number1 === null) {
+    number1 = number;
+    input.value = number1;
+  } else if (operation === null) {
+    number1 += number;
+    input.value = number1;
+  } else if (number2 === null) {
+    number2 = number;
+    input.value = number2;
+  }
+}
 
-    calculatorDiv.appendChild(buttonsDiv);
+function isOperation(buttonType) {
+  return (
+    buttonType === "+" ||
+    buttonType === "-" ||
+    buttonType === "X" ||
+    buttonType === "/"
+  );
+}
 
-    return buttons;
-};
+function isNumber(buttonType) {
+  return (
+    buttonType === "0" ||
+    buttonType === "1" ||
+    buttonType === "2" ||
+    buttonType === "3" ||
+    buttonType === "4" ||
+    buttonType === "5" ||
+    buttonType === "6" ||
+    buttonType === "7" ||
+    buttonType === "8" ||
+    buttonType === "9"
+  );
+}
+
+function getButtonType(buttonType) {
+  if (isOperation(buttonType)) {
+    return "operator";
+  } else if (isNumber(buttonType)) {
+    return "number";
+  } else if (buttonType === "as") {
+    return "clear";
+  } else if (buttonType === "=" || buttonType === "Enter") {
+    return "result";
+  } else {
+    throw new Error("Unknown button type");
+  }
+}
+
+function updateResult() {
+  input.value = getResult(number1, number2, operation);
+  number1 = input.value * 1;
+  number2 = null;
+}
+
+function onButtonPress(buttonType) {
+  if (getButtonType(buttonType) === "operator" && number1 && number2) {
+    updateResult();
+  }
+
+  switch (getButtonType(buttonType)) {
+    case "number":
+      onNumberPressed(buttonType);
+      break;
+    case "operator":
+      setOperation(buttonType);
+      break;
+    case "clear":
+      clearAll();
+      break;
+    case "result":
+      updateResult();
+      break;
+  }
+
+  //   console.log(number1, operation, number2);
+}
+
+function setOperation(buttonType) {
+  operation = buttonType;
+}
 
 function clearAll() {
-    input.value = 0;
-    a = '';
-    b = '';
-    sign = '';
-    finish = false;
-};
+  input.value = "";
+  number1 = null;
+  number2 = null;
+  operation = null;
+}
 
-function onPress(value) {
-    switch (value) {
-        case "=":
-            getResult();
-            break;
-        case "as":
-            clearAll();
-            break;
-        case "+":
-        case "-":
-        case "X":
-        case "/":
-            input.value = value
-            sign = value;
-            break
-        case "1":
-        case "2":
-        case "3":
-        case "4":
-        case "5":
-        case "6":
-        case "7":
-        case "8":
-        case "9":
-        case "0":
-            if (b == '' && sign === '') {
-                a += value;
-                input.value = a;
-            }
-            else if (a !== '' && b !== '' && finish) {
-                b = value;
-                finish = false;
-                input.value = b;
-            }
-            else {
-                b += value;
-                input.value = b;
-            }
-            break
-    }
-    
-    console.log(a, sign,b );
-};
+function getResult(value1, value2, operation) {
+  let res;
+  value1 *= 1;
+  value2 *= 1;
 
-function getResult() {
-    switch (sign) {
-        case "+/-":
-            break;
-        case "+":
-            a = (+a) + (+b);
-            input.value = a;
-            break
-        case "-":
-            a = (+a) - (b);
-            input.value = a;
-            break
-        case "X":
-            a = (+a) * (b);
-            input.value = a;
-            break
-        case "/":
-            a = (+a) / (b);
-            input.value = a;
-            break
-    }
-};
+  switch (operation) {
+    case "+":
+      res = value1 + value2;
+      break;
+    case "-":
+      res = value1 - value2;
+      break;
+    case "X":
+      res = value1 * value2;
+      break;
+    case "/":
+      res = value1 / value2;
+      break;
+  }
 
-function inputChange() {
-    if (a == '' && b == '') {
-        a += input.value
-    } else if (b == '' && a !== '' && sign !== '') {
-        b += input.value
-    } else if (a !== '' && b !== '' && sign !== '') {
-        clearAll();
-        a += input.value
-    }
-};
-
-
-
-let a = '';
-let b = '';
-let sign = '';
-let finish = false;
+  return res;
+}
 
 const calculatorDiv = createMainDiv();
+document.body.appendChild(calculatorDiv);
+
 const input = createInput();
-const buttons = createButtons();
+calculatorDiv.appendChild(input);
+document.onkeydown = (event) => onButtonPress(event.key);
+
+const buttonsDiv = createButtonsDiv();
+calculatorDiv.appendChild(buttonsDiv);
+
+createButtons(buttonsList, buttonsDiv);
